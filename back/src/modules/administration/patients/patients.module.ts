5ecb252b4edc1ca836a37737c9586460bda203bd
Jonathan Bohorquez <bohorquezvillamiljonathan@gmail.com>
2024-08-27 22:58:38 -0500

    Validacion en eliminar

--- back/src/modules/administration/patients/patients.module.ts
+++ back/src/modules/administration/patients/patients.module.ts
@@ -4,10 +4,11 @@ import { PatientsController } from './patients.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Patient } from './entities/patient.entity';
 import { JwtStrategy } from 'src/jwt/jwt.strategy';
+import { ExamPatient } from '../exams-patients/entities/exams-patient.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Patient]),
+    TypeOrmModule.forFeature([Patient, ExamPatient]),
   ],
   controllers: [PatientsController],
   providers: [PatientsService, JwtStrategy],

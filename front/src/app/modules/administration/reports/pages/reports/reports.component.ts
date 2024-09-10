83fb5b984fef9a1a5fe4896dfa05cfe63567e71a
Edwin Andrey Gaviria <edwin.gaviria@enequipo.co>
2024-09-09 22:22:46 -0500

    fix: adjustments reports module

--- front/src/app/modules/administration/reports/pages/reports/reports.component.ts
+++ front/src/app/modules/administration/reports/pages/reports/reports.component.ts
@@ -1,27 +1,58 @@
-import { Component } from '@angular/core';
-import { UntypedFormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup } from '@angular/forms';
+import { ReportsService } from '../../services/reports.service';
+import { RestService } from 'src/app/shared/services/rest.service';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
-export class ReportsComponent {
+export class ReportsComponent implements OnInit {
   form!: UntypedFormGroup;
-  public getInventoryReports() {
-    // this._inventoryReportsSvc.setParams();
+  reports: any;
+  loading: boolean = false;
+  reportDialog: boolean = false;
+  report: any = {};
 
-    // this._restSvc.methodPost(this.form.value, '').subscribe((res: any) => {
-    //     this.inventoryReports = res.map(element => {
-    //         if (element.date) {
-    //             const dateParts = element.date.split('-');
-    //             element.dateString = new Date(+dateParts[0], +dateParts[1] - 1, +dateParts[2]);
-    //         } else {
-    //             element.dateString = null;
-    //         }
-    //         return element;
-    //     });
-    //     this.loading = false;
-    // });
-}
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly _reportsSvc: ReportsService,
+    private readonly _restSvc: RestService,
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      initDate: [ null ],
+      endDate: [ new Date() ],
+    });
+    this.getReports();
+  }
+
+  public getReports() {
+    this._reportsSvc.setParams();
+
+    if (!this.form.get('initDate').value || !this.form.get('endDate').value) return;
+    this.loading = true;
+
+    this._restSvc.methodPost(this.form.value, 'exams').subscribe((res: any) => {
+      this.reports = res.data;
+      this.loading = false;
+    });
+  }
+
+  seeItem(report: any) {
+    this.reportDialog = true;
+    this.report = { ...report };
+  }
+
+  hideDialog() {
+    this.report = {};
+    this.reportDialog = false;
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
 }
